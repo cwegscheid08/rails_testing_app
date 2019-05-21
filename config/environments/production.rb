87e5f00856f820f1aa4105095446a0fef99f6f68
Chris Wegscheid <cwegscheid08@outlook.com>
2019-05-21 10:03:21 -0400

    Add account activation_2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,11 +71,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address          => 'smtp.sendgrid.net',
-    :port             => '587'
-    :authentication   => :plain
+    :port             => '587',
+    :authentication   => :plain,
     :user_name        => ENV['SENDGRID_USERNAME'],
     :password         => ENV['SENDGRID_PASSWORD'],
-    :domain           => 'heroku.com'
+    :domain           => 'heroku.com',
     :endable_starttls_auto => true
   }
 
